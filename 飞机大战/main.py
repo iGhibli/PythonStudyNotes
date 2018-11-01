b72346cdf1a763ea44b29ffb3bfd72a96ccb37a3
iGhibli <iGhibli@163.com>
2018-11-01 21:30:24 +0800

    添加精灵和精灵组

--- 飞机大战/main.py
+++ 飞机大战/main.py
@@ -1,4 +1,5 @@
 import pygame
+from plane_sprites import *
 
 # 初始化pygame
 pygame.init()
@@ -23,11 +24,28 @@ clock = pygame.time.Clock()
 # 定义飞机初始位置
 hero_rect = pygame.Rect(190, 700, 100, 124)
 
+# 创建游戏精灵
+enemy = GameSprite("./image/enemy1.png")
+# 创建游戏精灵组
+enemy_group = pygame.sprite.Group(enemy)
+
 
 # 游戏循环
 while True:
-    # 指定循环体内部代码的执行频率
+
+    # 设置屏幕刷新帧率
     clock.tick(60)
+
+    # 事件监听
+    for event in pygame.event.get():
+
+        # 判断用户是否点击了关闭按钮
+        if event.type == pygame.QUIT:
+            print("退出游戏...")
+            pygame.quit()
+
+            # 直接退出系统
+            exit()
 
     # 改变飞机位置
     if hero_rect.y < 0:
@@ -39,12 +57,12 @@ while True:
     screen.blit(bg, (0, 0))
     screen.blit(hero, hero_rect)
 
+    enemy_group.update()
+    enemy_group.draw(screen)
+
     # 更新显示
     pygame.display.update()
 
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        pygame.quit()
-        exit()
 
-pygame.quit()
+
+
