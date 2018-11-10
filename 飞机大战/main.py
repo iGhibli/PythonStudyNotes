62d5419e4e19e80fa23cca9f216fe845ed928d45
iGhibli <iGhibli@163.com>
2018-11-10 18:11:35 +0800

    临时

--- 飞机大战/main.py
+++ 飞机大战/main.py
@@ -1,6 +1,64 @@
 import pygame
-from plane_sprites import *
 
+# 屏幕大小的常量
+SCREEN_RECT = pygame.Rect(0, 0, 480, 852)
+# 游戏刷新帧率
+FRAME_PER_SEC = 60
+
+
+class PlaneGame(object):
+    """飞机大战主游戏"""
+    def __init__(self):
+        # 1. 创建游戏的窗口
+        self.screen = pygame.display.set_mode(SCREEN_RECT.size)
+        # 2. 创建游戏的时钟
+        self.clock = pygame.time.Clock()
+        # 3. 调用私有方法，精灵和精灵组的创建
+        self.__create_sprites()
+
+    def __create_sprites(self):
+        pass
+
+    def start_game(self):
+        """游戏开始"""
+        while True:
+            # 1. 设置刷新帧率
+            self.clock.tick(FRAME_PER_SEC)
+            # 2. 事件监听
+            self.__event_handler()
+            # 3. 碰撞检测
+            self.__check_collide()
+            # 4. 更新、绘制精灵组
+            self.__update_sprites()
+            # 5. 更新显示
+            pygame.display.update()
+
+            pass
+
+    def __event_handler(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                PlaneGame.__game_over()
+
+    def __check_collide(self):
+        pass
+
+    def __update_sprites(self):
+        pass
+
+    @staticmethod
+    def __game_over():
+        """游戏结束"""
+        pygame.quit()
+        exit()
+
+
+if __name__ == '__main__':
+    game = PlaneGame()
+    game.start_game()
+
+
+"""
 # 初始化pygame
 pygame.init()
 
@@ -63,7 +121,6 @@ while True:
 
     # 更新显示
     pygame.display.update()
-
-
+"""
 
 
--- 飞机大战/plane_sprites.py
+++ 飞机大战/plane_sprites.py
@@ -1,5 +1,8 @@
 import pygame
 
+# 屏幕大小的常量
+SCREEN_RECT = pygame.Rect(0, 0, 480, 852)
+
 
 class GameSprite(pygame.sprite.Sprite):
     """游戏精灵"""
@@ -14,3 +17,15 @@ class GameSprite(pygame.sprite.Sprite):
     def update(self):
 
         self.rect.y += self.speed
+
+
+class Background(GameSprite):
+    """游戏背景精灵"""
+
+    def update(self):
+        # 1. 调用父类的方法实现
+        super().update()
+
+        # 2. 判断是否移出屏幕，如果移出屏幕，将图像设置到屏幕的上方
+        if self.rect.y > SCREEN_RECT.height:
+            self.rect.y = -self.rect.height
